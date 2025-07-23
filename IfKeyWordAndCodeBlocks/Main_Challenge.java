e93a53d00a646fd91f07e562cad7f93677574440
Radha1253 <rajninitturi@gmail.com>
2025-07-23 12:58:24 -0500

    Challenge Question done

---
+++ IfKeyWordAndCodeBlocks/Main_Challenge.java
@@ -0,0 +1,20 @@
+public class Main_Challenge{
+    public static void main(String[] args) {
+        
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        int finalScore = score;
+         if (gameOver == true){
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+         }
+ 
+    }
+}
