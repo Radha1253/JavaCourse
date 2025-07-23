8f4d8a70da02aae5ec62b05569ba0ec9eb271f02
Radha1253 <rajninitturi@gmail.com>
2025-07-23 13:51:13 -0500

    Advanced Techniques done

--- IfKeyWordAndCodeBlocks/Main_Challenge.java
+++ IfKeyWordAndCodeBlocks/Main_Challenge.java
@@ -15,6 +15,19 @@ public class Main_Challenge{
             finalScore += (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
          }
+         
+         calculateScore(true, 800, levelCompleted,bonus);
  
     }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+         if (gameOver){
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+         }
+    }
+
+    
 }
