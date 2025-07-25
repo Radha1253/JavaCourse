e5f84a4c00f3b10d12be59b202b1fab1207d0063
Radha1253 <rajninitturi@gmail.com>
2025-07-25 10:23:11 -0500

    challenges

---
+++ CodingExercises/PositiveNegativeZero.java
@@ -0,0 +1,11 @@
+public class PositiveNegativeZero{
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("positive");
+        }
+        if(number < 0){
+            System.out.println("negative");
+        }
+        System.out.println("zero");
+    }
+}
